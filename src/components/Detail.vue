d047a86f4bf8cf4d9d7c17adfc50a78f73e911c5
FattahSalleh <fattah.sallehuddin@gmail.com>
2021-01-16 13:18:24 +0800

    Detail.vue addition of some functions

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,5 +1,22 @@
 <template>
 
+    <div id="detail">
 
+        <button class ="back-button">
+            <i class="fas fa-arrow-left"></i>
+        </button>
+
+        <main>
+        
+        <div class="">
+        
+
+        
+        </div>
+        
+        </main>
+
+
+    </div>
 
 </template>
